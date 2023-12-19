f3173e19e06ec74e981822075b8eb8b5e791115a
qurbonovozodbek <ozodbekkurbonov185@gmail.com>
2023-12-19 15:43:49 +0500

    funksiya

---
+++ app.js
@@ -0,0 +1,238 @@
+// 1 - masala
+
+// function sum(a = 10 , b = 23, c = 76) { 
+//     let max;
+//     if (a > b) {
+//         max = a;
+//     } else {
+//         max = b;
+//     }
+//     if (max < c) {
+//         max = c;
+//     }
+
+//     return max;
+// }
+// console.log(sum());
+
+
+// 2 - massala
+
+//  let a = 15;
+
+//  switch (a) {
+//     case 10:
+//     case 11:
+//     case 12:
+//     case 13:
+//     case 14:
+//     case 15:
+//     case 16:
+//     case 17:
+//     case 18: 
+//     case 19:
+//         console.log(`Birinchi o'nlikda`);
+//         break;
+
+//     case 20:
+//     case 21:
+//     case 22:
+//     case 23:
+//     case 24:
+//     case 25:
+//     case 26:
+//     case 27:
+//     case 28: 
+//     case 29:
+//         console.log(`Ikkinchi o'nlikda`);
+//         break;
+
+//     case 30:
+//     case 31:
+//     case 32:
+//     case 33:
+//     case 34:
+//     case 35:
+//     case 36:
+//     case 37:
+//     case 38: 
+//     case 39:
+//         console.log(`Uchinchi o'nlikda`);
+//         break;
+
+//     case 40:
+//     case 41:
+//     case 42:
+//     case 43:
+//     case 44:
+//     case 45:
+//     case 46:
+//     case 47:
+//     case 48: 
+//     case 49:
+//         console.log(`To'rtinchi o'nlikda`);
+//         break;
+
+//     case 50:
+//     case 51:
+//     case 52:
+//     case 53:
+//     case 54:
+//     case 55:
+//     case 56:
+//     case 57:
+//     case 58: 
+//     case 59:
+//         console.log(`Beshinchi o'nlikda`);
+//         break;
+
+//     case 60:
+//     case 61:
+//     case 62:
+//     case 63:
+//     case 64:
+//     case 65:
+//     case 66:
+//     case 67:
+//     case 68: 
+//     case 69:
+//         console.log(`Oltinchi o'nlikda`);
+//         break;
+
+//     case 70:
+//     case 71:
+//     case 72:
+//     case 73:
+//     case 74:
+//     case 75:
+//     case 76:
+//     case 77:
+//     case 78: 
+//     case 79:
+//         console.log(`Yettinchi o'nlikda`);
+//         break;
+
+//     case 80:
+//     case 81:
+//     case 82:
+//     case 83:
+//     case 84:
+//     case 85:
+//     case 86:
+//     case 87:
+//     case 88: 
+//     case 89:
+//         console.log(`Sakizinchi o'nlikda`);
+//         break;
+
+//     case 90:
+//     case 91:
+//     case 92:
+//     case 93:
+//     case 94:
+//     case 95:
+//     case 96:
+//     case 97:
+//     case 98: 
+//     case 99:
+//         console.log(`To'qizzinchi o'nlikda`);
+//         break;
+
+//     default:
+//         console.log(`Siz ikki xonali son kiritmadingiz!`);
+//         break;
+//  }
+
+
+// 3 - masala
+
+// function son(n=8) {
+//     let counter = 0;
+//     let i = 1;
+//     while (i <= n) {
+//         if (i % 2 == 1) {
+//             counter++;
+//         }
+//         i++;
+//     }
+//     return counter;
+// }
+// console.log(son());
+
+
+// 4 - masala
+
+// function sum(a=120) {
+//     let counter = 0;
+//     let i = 1
+//     for (i; i <= a; i++) {
+//         if (a % i == 0) {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+// console.log(sum());
+
+
+// 5 - masala 
+
+// function sum(a=16) {
+//     let max = 1;
+//     if (a % 3 == 0 && a % 5 == 0) {
+//         max = a ** 2;
+//     } else {
+//         max = a ** 3;
+//     }
+//     return max;
+// }
+// console.log(sum());
+
+
+// 6 - masala 
+
+// function sum(a = 43, b = 33) {
+//     let aBir = Math.trunc(a / 10)
+//     let aIkki = Math.trunc(a % 10)
+//     let aYigindi = (aBir*aIkki)
+
+//     let bBir = Math.trunc(b / 10)
+//     let bIkki = Math.trunc(b % 10)
+//     let bYigindi = (bBir*bIkki)
+
+//     let max; 
+//     if (aYigindi > bYigindi) {
+//         max = aYigindi;
+//     } else {
+//         max = bYigindi;
+//     }
+//     return max;
+// }
+// console.log(sum());
+
+
+// 7 - masala
+
+    // function sum(a=11) {
+
+    //     let counter = 0;
+    //     let i = 1
+
+    //     for (i; i <= a; i++) {
+    //         if (a % i == 0) {
+    //             counter++;
+    //         }
+    //     }
+
+    //     let max;
+
+    //     if (counter == 2) {
+    //         max = "tub son"
+    //     } else {
+    //         max = "tub son emas"
+    //     }
+
+    //     return max;
+    // }
+
+    // console.log(sum());
